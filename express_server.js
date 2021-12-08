eea846b5c99c629919351d04d7ad86d02f2ab613
Suliman Fayyaz <hydrajin@gmail.com>
2021-12-08 01:06:44 -0500

    added route handler & basic HTML skeleton

--- express_server.js
+++ express_server.js
@@ -27,9 +27,22 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+// add a new rout handler for "/urls" and use res.render() to pass the URL data to our template
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+// templateVars object contains the object urlDatabase? under the key urls
+// We then pass the templateVars object to the template calls urls_index
+
+
 // * Visit: localhost:8080/urls.json {"b2xVn2":"http://www.lighthouselabs.ca","9sm5xK":"http://www.google.com"}
 // Use cURL to fetch the url: curl -i http://localhost:8080/hello
-
+ 
 // * Will a variable that is created in one request be accessible in another?
 /* a is not accessible in the other function/callback. The user will NOT see a set to 1 in /fetch.
 In fact, a is not defined in this scope, and will result in a reference error when anyone visits that URL. */
+
+// * Express convention of using views (EJS automatically knows to look inside the views directory for any template files with .ejs extension)
+/* When sending variables to an EJS template, we need to send them inside an object, even if we are only sending one variable.
+ This is so we can use the key of that variable (in the above case the key is urls) to access the data within our template. */
