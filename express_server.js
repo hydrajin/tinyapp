84caf3731abd427ad332038297b71ce17d74ba49
Suliman Fayyaz <hydrajin@gmail.com>
2021-12-07 21:30:34 -0500

    Added response with HTML code

--- express_server.js
+++ express_server.js
@@ -16,8 +16,13 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
 // * Visit: localhost:8080/urls.json {"b2xVn2":"http://www.lighthouselabs.ca","9sm5xK":"http://www.google.com"}
+// Use cURL to fetch the url: curl -i http://localhost:8080/hello
